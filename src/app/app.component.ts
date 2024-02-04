d25981aa540276c08cd5f1c22148a573ca20eacc
Adriana Del Teso <adrianadelteso@gmail.com>
2024-02-04 20:14:55 +0100

    :construction: update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from '../components/user/user.component';
+import { GamesComponent } from '../components/games/games.component';
 
 // el decorador es una sintaxis y en patrón, que typescript implementa con JS.
 // @Component afecta a la clase AppComponent
@@ -10,10 +12,11 @@ import { RouterOutlet } from '@angular/router';
   // si queremos utilizar cualquier componente o cualquier módulo
   // ahora hay que ponerlo en "imports" de la siguiente línea
   // standalone previene mejor el código repetido
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, UserComponent, GamesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Angular 17 Playground';
+  city = 'Madrid';
 }
