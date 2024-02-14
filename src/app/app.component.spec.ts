65ae80a7049163512ff18a10cc5293cce2cab63a
Adriana Del Teso <adrianadelteso@gmail.com>
2024-02-14 18:39:50 +0100

    :art: update app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-17-playground' title`, () => {
+  it(`should have the 'board-games-showcase' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-17-playground');
+    expect(app.title).toEqual('board-games-showcase');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-17-playground');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, board-games-showcase',
+    );
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { UserComponent } from '../components/user/user.component';
 import { GamesComponent } from '../components/games/games.component';
 
@@ -12,11 +18,27 @@ import { GamesComponent } from '../components/games/games.component';
   // si queremos utilizar cualquier componente o cualquier módulo
   // ahora hay que ponerlo en "imports" de la siguiente línea
   // standalone previene mejor el código repetido
-  imports: [CommonModule, RouterOutlet, UserComponent, GamesComponent],
+  imports: [
+    CommonModule,
+    GamesComponent,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenav,
+    MatSidenavModule,
+    MatToolbarModule,
+    RouterOutlet,
+    UserComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Angular 17 Playground';
-  city = 'Madrid';
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+
+  reason = '';
+
+  close(reason: string) {
+    this.reason = reason;
+    this.sidenav.close();
+  }
 }
