bd91dafcb081bd502460c71ca92803b82c3b42d3
Adriana Del Teso <adrianadelteso@gmail.com>
2024-03-23 13:42:54 +0100

    :art: add scroll-to-top-btn to oracles

--- src/components/oracles/oracles.component.ts
+++ src/components/oracles/oracles.component.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { HighlightTextPipe } from '../../core/pipes/highlight-text/highlight-text.pipe';
 import { CommonFunctionsService } from '../../core/functions/common/common-functions.service';
 import { FilterFunctionsService } from '../../core/functions/filter/filter-functions.service';
+import { ScrollToTopBtnComponent } from '../scroll-to-top-btn/scroll-to-top-btn.component';
 
 @Component({
   selector: 'app-oracles',
   standalone: true,
-  imports: [CommonModule, HighlightTextPipe],
+  imports: [CommonModule, HighlightTextPipe, ScrollToTopBtnComponent],
   templateUrl: './oracles.component.html',
   styleUrl: '../common-styles.scss',
 })
