3ae8f0bc99e1c50a14ebbee73f8930bd5fd9a3d0
Adriana Del Teso <adrianadelteso@gmail.com>
2024-03-26 11:01:34 +0100

    :construction: WIP add HTTP service

---
+++ src/core/services/http.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GameCard, OracleCard } from '../../components/commons.models';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpService {
+  constructor(private http: HttpClient) {}
+
+  getGamesData(): Observable<{ games: GameCard[] }> {
+    return this.http.get<{ games: GameCard[] }>('src/assets/data/games.json');
+  }
+
+  getOraclesData(): Observable<{ oracles: OracleCard[] }> {
+    return this.http.get<any>('src/assets/data/oracles.json');
+  }
+}
