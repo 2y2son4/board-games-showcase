6cdf466179a576798a7920959b53808378329402
Adriana Del Teso <adrianadelteso@gmail.com>
2026-05-26 21:15:59 +0200

    :zap: modernize app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,5 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
 export const routes: Routes = [{ path: '', component: AppComponent }];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
