364dec04e2203e314590ca34c5fe3cc07efe8ac9
Adriana Del Teso <adrianadelteso@gmail.com>
2026-05-28 20:14:20 +0200

    :recycle: refactor app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,13 @@
 import { Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { GamesComponent } from '../components/games/games.component';
+import { OraclesComponent } from '../components/oracles/oracles.component';
+import { BggSearchComponent } from '../components/bgg-search/bgg-search.component';
 
-export const routes: Routes = [{ path: '', component: AppComponent }];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'games' },
+  { path: 'games', component: GamesComponent },
+  { path: 'oracles', component: OraclesComponent },
+  { path: 'search', component: BggSearchComponent },
+  { path: '**', redirectTo: 'games' },
+];
